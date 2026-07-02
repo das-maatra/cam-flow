f7948faa83949f0815632feb5f9a54cf6d8b3733
Shuvo Das <maatra.connect@gmail.com>
2026-07-02 21:40:58 +0100

    request camera fees ad display it full screen

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-document.body.innerText = 'cam-flow is alive';
+const video = document.getElementById('camera');
+
+try {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { facingMode: 'environment' },
+  });
+  video.srcObject = stream;
+  await video.play();
+} catch (err) {
+  document.body.innerText = `Camera error: ${err.message}`;
+}
